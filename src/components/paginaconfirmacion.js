cf44c32af50a520edd82a0c81f8335f9fc341980
WendyChavez <wendychavez8520@gmail.com>
2020-09-07 11:23:18 -0500

    Modificacion

--- src/components/paginaconfirmacion.js
+++ src/components/paginaconfirmacion.js
@@ -10,7 +10,7 @@ export const paginaconfirmacion = () => {
     return (
 
 
-        <div class="other myDiv">
+        <div class="other fondo">
             <br />
             <br />
             <br />
@@ -19,24 +19,27 @@ export const paginaconfirmacion = () => {
                     <div class="col-xl-6 col-lg-7 col-md-9">
                         <div class="card shadow-lg">
                             <div class="card-body p-4 p-md-5 centrado">
+                                <b><p class="registro"> REGISTRO EXITOSO</p></b>
+
+                                <b><p class="letra"> Ahora su cuenta esta lista para usarse</p></b>
+                                <br />
+
+                                <br />
                                 <b><p class="registro"> Cad&Lan</p></b>
                                 <img src={cad} width="150" height="150" className="rounded mx-auto d-block mt-4 mb-3" alt="" loading="lazy" />
                                 <br />
+
                                 <br />
-                                <a href="" class="btn btn-primary btn-lg">REGISTRO EXITOSO</a>
-                                <br />
-                                <br />
-                                <p>Ahora su cuenta esta lista para usarse</p>
                                 <br />
                                 <br />
                             </div>
                         </div>
                         <br />
-                        
-                                <br />
-                                <br />
-                                <br />
-                                
+
+                        <br />
+                        <br />
+
+                        <br />
                     </div>
                 </div>
             </div>
