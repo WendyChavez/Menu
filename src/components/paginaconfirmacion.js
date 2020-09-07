18d83ce3725dc547d4af6a1eb21e7a3e993a01fb
WendyChavez <wendychavez8520@gmail.com>
2020-09-07 12:00:56 -0500

    Modificacion tipografia

--- src/components/paginaconfirmacion.js
+++ src/components/paginaconfirmacion.js
@@ -20,25 +20,20 @@ export const paginaconfirmacion = () => {
                         <div class="card shadow-lg">
                             <div class="card-body p-4 p-md-5 centrado">
                                 <b><p class="registro"> REGISTRO EXITOSO</p></b>
-
                                 <b><p class="letra"> Ahora su cuenta esta lista para usarse</p></b>
                                 <br />
-
+                                <br />
                                 <br />
                                 <b><p class="registro"> Cad&Lan</p></b>
                                 <img src={cad} width="150" height="150" className="rounded mx-auto d-block mt-4 mb-3" alt="" loading="lazy" />
                                 <br />
-
                                 <br />
                                 <br />
                                 <br />
                             </div>
                         </div>
                         <br />
-
                         <br />
-                        <br />
-
                         <br />
                     </div>
                 </div>
